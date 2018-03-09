f9179fde1a02e5932be70ac6397c2b328097e8af
Rui  Vieira <ruidevieira@googlemail.com>
2018-03-09 20:09:53 +0000

    Added missing G in state advance

--- src/main/java/org/ruivieira/jssm/univariate/StageGenerator.java
+++ src/main/java/org/ruivieira/jssm/univariate/StageGenerator.java
@@ -5,6 +5,8 @@ import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealVector;
 import org.ruivieira.jssm.common.Structure;
 
+import static org.apache.commons.math3.linear.MatrixUtils.createRealVector;
+
 public class StageGenerator {
 
     public static RealVector[] states(int nobs,
@@ -19,13 +21,23 @@ public class StageGenerator {
             if (i == 0) {
                 mean = state0.toArray();
             }  else {
-                mean = states[i - 1].toArray();
+                mean = structure.getG().preMultiply(states[i - 1]).toArray();
             }
             final double[][] covariance = structure.getW().getData();
             states[i] = new ArrayRealVector(new MultivariateNormalDistribution(mean, covariance).sample());
         }
 
         return states;
+    }
+
+    public static RealVector advance(RealVector state,
+                                     Structure structure) {
+
+        final double[] mean = structure.getG().preMultiply(state).toArray();
+        final double[] mean1 = new MultivariateNormalDistribution(mean, structure.getW().getData()).sample();
+
+        return createRealVector(mean1);
+
     }
 
 }
